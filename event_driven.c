304e06288b41c09eeedd5c55e509373e934f01be
Ian Baldo <ianblopes@gmail.com>
2014-08-28 17:56:08 -0300

    first commit

---
+++ event_driven.c
@@ -0,0 +1,65 @@
+// event_driven.c
+
+// TIMER
+unsigned int timer1;
+unsigned int timer2;
+bool timer1_active;
+bool timer2_active;
+
+
+// BUTTON
+bool button_active;
+bool previous_state;
+int BUT_PIN;
+
+
+void button_listen(int pin) {
+
+	pinMode(pin,INPUT);
+	BUT_PIN = pin;
+}
+
+void timer_set (int ms) {
+	if(!timer1_active){
+		timer1 = millis() + ms;
+		timer1_active = 1;
+	}
+	if(!timer2_active){
+		timer2 = millis() + ms;
+		timer2_active = 1;
+	}
+}
+
+void button_changed (int pin, int v);
+void timer_expired ();
+
+
+void setup () {
+	timer1_active = 0;
+	timer2_active = 0;
+	button_active = 0;
+	previous_state = 0;
+	init();	
+}
+
+void loop () {
+	
+	if (button_active){
+		bool current_state = digitalRead(BUT_PIN);
+		if (current_state != previous_state){
+			button_changed (BUT_PIN, current_state);
+			previous_state = current_state;
+		}
+	}
+
+	if (timer1_active)				// testa se existe timer ativo
+		if (millis() >= timer1){		// testa se passou o tempo do timer
+			timer1_expired();
+			timer1_active = 0;
+		}
+	if (timer2_active)			
+		if (millis() >= timer2){		
+			timer2_expired();
+			timer2_active = 0;
+		}
+}
