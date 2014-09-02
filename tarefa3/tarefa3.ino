8139c8184f14a6316a16f5adc30ebb613b001ae8
Ian Baldo <ianblopes@gmail.com>
2014-09-02 18:06:12 -0300

    final

--- tarefa3/tarefa3.ino
+++ tarefa3/tarefa3.ino
@@ -7,8 +7,10 @@
 // TIMER
 unsigned int timer1;
 unsigned int timer2;
-bool timer1_active;
-bool timer2_active;
+unsigned int ms1;
+unsigned int ms2;
+int timer1_active;
+int timer2_active;
 
 
 // BUTTON
@@ -24,14 +26,14 @@ void button_listen(int pin) {
 }
 
 void timer1_set (int ms) {
-	
-	timer1 = millis() + ms;
+	timer1 = millis();
+        ms1 = ms;
 	timer1_active = 1;
 	
 }
 void timer2_set (int ms) {
-
-	timer2 = millis() + ms;
+        ms2 = ms;
+	timer2 = millis();
 	timer2_active = 1;
 }
 
@@ -41,6 +43,7 @@ void timer2_expired ();
 
 
 void setup () { 
+        Serial.begin(9600);
 	timer1_active = 0;
 	timer2_active = 0;
 	buttons = 0;
@@ -53,57 +56,64 @@ void loop () {
 	if (buttons > 0){
 		int i;
 		for(i=0;i<buttons;i++) {
-			bool current_state = digitalRead(button_pin[i]);
+			int current_state = digitalRead(button_pin[i]);
 			if (current_state != previous_state[i]){
 				button_changed (button_pin[i], current_state);
+                                Serial.print("ENTREI BOTAO -> ");
+                                Serial.print(button_pin[i]);
+                                Serial.print(" ");
+                                Serial.println(i);
 				previous_state[i] = current_state;
 			}
 		}
 	}
-
-	if (timer1_active)				// testa se existe timer ativo
-		if (millis() >= timer1){		// testa se passou o tempo do timer
-			timer1_active = 0;			
+	if (timer1_active){			
+		unsigned int dt1 = millis() - timer1;
+  //              Serial.print("dt1 = ");
+   //             Serial.println(dt1);
+                if (dt1 >= ms1){		
+                        timer1_active = 0;			
 			timer1_expired();			
 		}
-	if (timer2_active)			
-		if (millis() >= timer2){		
+        }
+	if (timer2_active){
+                unsigned int dt2 = millis() - timer2;			
+		if (dt2 >= ms2){		
 			timer2_active = 0;			
 			timer2_expired();			
 		}
+        }
 }
 
 #define LED1_PIN 13
 #define LED2_PIN 12
-#define BUT1_PIN 1
+#define BUT1_PIN 7
 #define BUT2_PIN 2
 
-int led_freq; 	// LED frequency
+unsigned int led_freq; 	// LED frequency
 int button1_pressed;
 int button2_pressed;
-int timer_active;
 int state;
 
 void init1() {
 	pinMode(LED1_PIN,OUTPUT);
-//	pinMode(LED2_PIN,OUTPUT);
+	pinMode(LED2_PIN,OUTPUT);
 	
-	button_listen(BUT1_PIN);
+        button_listen(BUT1_PIN);
 	button_listen(BUT2_PIN);
 
 	led_freq = 1000;
 	button1_pressed = 0;
 	button2_pressed = 0;
-        timer_active = 0;
         state = 0;
         
 	timer1_set(led_freq);
 }
 
 void button_changed(int pin, int v){
-	if (pin == BUT1_PIN){
+	if (pin == BUT1_PIN && v == 0){
 		// button 1
-		led_freq = led_freq + 100;
+		led_freq = led_freq + 1000;
 		button1_pressed = 1;
 
 		if (button2_pressed)
@@ -112,9 +122,11 @@ void button_changed(int pin, int v){
 			timer2_set(500);
 		}
 	}
-	else {
+	else if (pin == BUT2_PIN && v == 0){
 		// button 2
-		led_freq = led_freq - 100;
+		led_freq = led_freq - 1000;
+                if (led_freq <= 0)
+                    led_freq = 1000;
 		button2_pressed = 1;
 
 		if (button1_pressed)
@@ -131,6 +143,8 @@ void timer2_expired(){
 }
 
 void timer1_expired(){
+        Serial.print("LED_freq -> ");
+        Serial.println(led_freq);
 	digitalWrite(LED1_PIN,state);
 	digitalWrite(LED2_PIN,state);
 	state = !state;
